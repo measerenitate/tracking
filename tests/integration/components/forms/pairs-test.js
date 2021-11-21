301a10bb6fc4a7c660ab6be98759eb3397133f24
Mea Serenitate <measerenitate@gmail.com>
2021-11-21 16:04:24 +0100

    tests(forms:pairs): add basic test coverage

--- tests/integration/components/forms/pairs-test.js
+++ tests/integration/components/forms/pairs-test.js
@@ -1,26 +1,113 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, fillIn, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | forms/pairs', function (hooks) {
   setupRenderingTest(hooks);
 
+  hooks.beforeEach(function() {
+    this.set('dummyAction', () => {});
+    this.set('isSubmitted', false);
+    this.set('pairs', [{
+      name: 'dessert',
+      negative: 'Chocolate Cake',
+      positive: 'Strawberry Cake',
+      rangeMin: 0,
+      rangeMax: 10,
+      value: 1,
+    }, {
+      name: 'breakfast',
+      negative: 'Bread',
+      positive: 'Toast',
+      rangeMin: 0,
+      rangeMax: 10,
+      value: 2,
+    }, {
+      name: 'lunch',
+      negative: 'Sandwich',
+      positive: 'SOS Roll',
+      rangeMin: 0,
+      rangeMax: 10,
+      value: 3,
+    }]);
+  });
+
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    await render(hbs`<Forms::Pairs @pairs={{this.pairs}} @onSubmit={{this.dummyAction}} @isSubmitted={{this.isSubmitted}} />`);
 
-    await render(hbs`<Forms::Pairs />`);
+    assert.dom('[data-test-forms-pairs]').exists({ count: 1 });
 
-    assert.dom(this.element).hasText('');
+    assert.dom('[data-test-forms-pairs-entry]').exists({ count: 3 });
+    assert.dom('[data-test-forms-pairs-entry="dessert"]').exists({ count: 1 });
+    assert.dom('[data-test-forms-pairs-entry="breakfast"]').exists({ count: 1 });
+    assert.dom('[data-test-forms-pairs-entry="lunch"]').exists({ count: 1 });
+    assert.dom('[data-test-forms-pairs-submit]').includesText('Save');
+  });
 
-    // Template block usage:
-    await render(hbs`
-      <Forms::Pairs>
-        template block text
-      </Forms::Pairs>
-    `);
+  test('it enables the submit button by default', async function (assert) {
+    await render(hbs`<Forms::Pairs @pairs={{this.pairs}} @onSubmit={{this.dummyAction}} @isSubmitted={{false}} />`);
 
-    assert.dom(this.element).hasText('template block text');
+    assert.dom('[data-test-forms-pairs-submit]').isNotDisabled();
+  });
+
+  test('it disables the submit button if the @isSubmitted flag is set', async function (assert) {
+    await render(hbs`<Forms::Pairs @pairs={{this.pairs}} @onSubmit={{this.dummyAction}} @isSubmitted={{true}} />`);
+
+    assert.dom('[data-test-forms-pairs-submit]').isDisabled();
+  });
+
+  test('it triggers the onSubmit action if the form is submitted', async function (assert) {
+    assert.expect(1);
+
+    this.set('onSubmit', (value) => {
+      assert.ok(value);
+    })
+    await render(hbs`<Forms::Pairs @pairs={{this.pairs}} @onSubmit={{this.onSubmit}} @isSubmitted={{this.isSubmitted}} />`);
+
+    await click('[data-test-forms-pairs-submit]');
+  });
+
+  test('it allows entering values and submitting them', async function (assert) {
+    assert.expect(4);
+
+    let expectedValues = [{
+      name: 'dessert',
+      negative: 'Chocolate Cake',
+      positive: 'Strawberry Cake',
+      rangeMin: 0,
+      rangeMax: 10,
+      value: '2',
+    }, {
+      name: 'breakfast',
+      negative: 'Bread',
+      positive: 'Toast',
+      rangeMin: 0,
+      rangeMax: 10,
+      value: '4',
+    }, {
+      name: 'lunch',
+      negative: 'Sandwich',
+      positive: 'SOS Roll',
+      rangeMin: 0,
+      rangeMax: 10,
+      value: '8',
+    }];
+
+    this.set('onSubmit', (value) => {
+      assert.deepEqual(value, expectedValues);
+    })
+
+    await render(hbs`<Forms::Pairs @pairs={{this.pairs}} @onSubmit={{this.onSubmit}} @isSubmitted={{this.isSubmitted}} />`);
+
+    await fillIn('[data-test-forms-pairs-entry="dessert"]', 2);
+    await fillIn('[data-test-forms-pairs-entry="breakfast"]', 4);
+    await fillIn('[data-test-forms-pairs-entry="lunch"]', 8);
+
+    assert.dom('[data-test-forms-pairs-entry="dessert"]').hasValue('2');
+    assert.dom('[data-test-forms-pairs-entry="breakfast"]').hasValue('4');
+    assert.dom('[data-test-forms-pairs-entry="lunch"]').hasValue('8');
+
+    await click('[data-test-forms-pairs-submit]');
   });
 });
