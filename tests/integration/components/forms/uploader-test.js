042652bc05ba8db926a9890435848ee495b16995
Mea Serenitate <measerenitate@gmail.com>
2021-11-21 16:36:54 +0100

    tests(forms/uploader): add basic test coverage

--- tests/integration/components/forms/uploader-test.js
+++ tests/integration/components/forms/uploader-test.js
@@ -1,26 +1,60 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, triggerEvent } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+
+import { a11yAudit } from 'ember-a11y-testing/test-support';
 
 module('Integration | Component | forms/uploader', function (hooks) {
   setupRenderingTest(hooks);
 
+  hooks.beforeEach(function() {
+    this.set('dummyAction', () => {});
+  });
+
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    await render(hbs`<Forms::Uploader @onUpload={{this.dummyAction}} />`);
 
-    await render(hbs`<Forms::Uploader />`);
+    assert.dom('[data-test-forms-uploader]').exists({ count: 1 });
+    assert.dom('[data-test-forms-uploader-input]').exists({ count: 1 });
+    assert.dom('[data-test-forms-uploader-instructions]').includesText('Update your entries by uploading your .csv');
+  });
 
-    assert.dom(this.element).hasText('');
+  test('it offers uploads of .csvs', async function (assert) {
+    await render(hbs`<Forms::Uploader @onUpload={{this.dummyAction}} />`);
 
-    // Template block usage:
-    await render(hbs`
-      <Forms::Uploader>
-        template block text
-      </Forms::Uploader>
-    `);
+    assert.dom('[data-test-forms-uploader-input]').hasAttribute('type', 'file');
+    assert.dom('[data-test-forms-uploader-input]').hasAttribute('accept', 'text/csv');
+  });
 
-    assert.dom(this.element).hasText('template block text');
+  test('it is accessible', async function (assert) {
+    await render(hbs`<Forms::Uploader @onUpload={{this.dummyAction}} />`);
+
+    await a11yAudit();
+    assert.ok(true, 'no a11y errors detected!');
+  });
+
+  test('it sends the @onUpload action on changes to the upload input element', async function (assert) {
+    assert.expect(1);
+
+    this.set('onUpload', async (value) => {
+      let parsedData = await value.file.text();
+      assert.equal(parsedData, 'Ember Rules!');
+    })
+
+    await render(hbs`<Forms::Uploader @onUpload={{this.onUpload}} />`);
+    await triggerEvent('[data-test-forms-uploader-input]', 'change', { files: [new Blob(['Ember Rules!'])] });
+  });
+
+  test('it displays the success message after a file has been uploaded', async function (assert) {
+    await render(hbs`<Forms::Uploader @onUpload={{this.dummyAction}} />`);
+
+    assert.dom('[data-test-forms-uploader-instructions]').exists();
+    assert.dom('[data-test-forms-uploader-success-message]').doesNotExist();
+
+    await triggerEvent('[data-test-forms-uploader-input]', 'change', { files: [new Blob(['Ember Rules!'])] });
+
+    assert.dom('[data-test-forms-uploader-instructions]').doesNotExist();
+    assert.dom('[data-test-forms-uploader-success-message]').includesText('has been uploaded');
   });
 });
